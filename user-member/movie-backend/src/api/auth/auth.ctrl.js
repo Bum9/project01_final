e3268054b33800345066d47c22502178609c3b3b
Bum <Bum9@github.com>
2022-10-28 13:07:31 +0900

    프로젝트 결과물

--- user-member/movie-backend/src/api/auth/auth.ctrl.js
+++ user-member/movie-backend/src/api/auth/auth.ctrl.js
@@ -1,127 +1,112 @@
 import Joi from 'joi';
 import User from '../../models/user';
 
+export const register = async (ctx) => {
+  // Request Body 검증하기
+  const schema = Joi.object().keys({
+    userId: Joi.string().alphanum().min(3).max(20).required(),
+    password: Joi.string().required(),
+    username: Joi.string().required(),
+    email: Joi.string().required(),
+    gender: Joi.string().required(),
+    nickname: Joi.string().required(),
+    add: Joi.string().required(),
+    tel: Joi.string().required(),
+  });
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
 
-
-export const register = async ctx => {
-    // Request Body 검증하기
-    const schema = Joi.object().keys({
-        userId: Joi.string()
-            .alphanum()
-            .min(3)
-            .max(20)
-            .required(),
-        password: Joi.string().required(),
-        username: Joi.string().required(),
-        email: Joi.string().required(),
-        gender: : Joi.string().required(),
-        nickname : Joi.string().required(),
-        add : Joi.string().required(),
-        tel :: Joi.string().required(),
+  const { userId, password, username, email, gender, nickname, add, tel } =
+    ctx.request.body;
+  try {
+    const exists = await User.findByUsername(username);
+    if (exists) {
+      ctx.status = 409; // Conflict
+      return;
+    }
+    const user = new User({
+      userId,
+      username,
+      email,
+      gender,
+      nickname,
+      add,
+      tel,
     });
-    const result = schema.validate(ctx.request.body);
-    if (result.error) {
-        ctx.status = 400;
-        ctx.body = result.error;
-        return;
-    }
+    await user.setPassword(password);
+    await user.save();
 
+    ctx.body = user.serialize();
 
-
-    const { userId, password,username,email,gender,nickname,add,tel } = ctx.request.body;
-    try {
-
-        const exists = await User.findByUsername(username);
-        if (exists) {
-            ctx.status = 409; // Conflict
-            return;
-        }
-        const user = new User({
-            userId,
-            username,
-            email,
-            gender,
-            nickname,
-            add,
-            tel
-
-        });
-        await user.setPassword(password);
-        await user.save();
-
-
-        ctx.body= user.serialize();
-
-        const token = user.generateToken();
-        ctx.cookies.set('access_token',token,{
-            maxAge :1000 *60*60*24*7,
-            httpOnly:true,
-        });
-
-    }catch(e){
-    ctx.throw(500,e);
-    }
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    });
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
+export const login = async (ctx) => {
+  // 로그인
+  const { userId, password } = ctx.request.body;
 
-
-export const login = async ctx => {
-    // 로그인
-    const {userId, password} = ctx.request.body;
-
-    if (!userId || !password) {
-        ctx.status = 401; // Unauthorized
-        return;
+  if (!userId || !password) {
+    ctx.status = 401; // Unauthorized
+    return;
+  }
+  try {
+    const user = await User.findByUsername(userId);
+    if (!user) {
+      ctx.status = 401;
+      return;
     }
-    try {
-        const user = await User.findByUsername(userId);
-        if (!user) {
-            ctx.status = 401;
-            return;
-        }
 
-        const valid = await user.checkPassword(password);
-        // 잘못된 비밀번호
-        if (!valid) {
-            ctx.status = 401;
-            return;
-        }
-        ctx.body= user.serialize();
-        const token = user.generateToken();
-        ctx.cookies.set('access_token',token,{
-            maxAge :1000 *60*60*24*7,
-            httpOnly:true,
-        });
-    }catch(e){
-        ctx.throw(500,e);
+    const valid = await user.checkPassword(password);
+    // 잘못된 비밀번호
+    if (!valid) {
+      ctx.status = 401;
+      return;
     }
+    ctx.body = user.serialize();
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    });
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
+export const check = async (ctx) => {
+  // 로그인 상태 확인
 
-export const check = async ctx => {
-    // 로그인 상태 확인
-
-    const { user } =ctx.state;
-    if(!user){
-        ctx.state =401;
-        return;
-    }
-    ctx.body =user;
-
+  const { user } = ctx.state;
+  if (!user) {
+    ctx.state = 401;
+    return;
+  }
+  ctx.body = user;
 };
 
-export const logout = async ctx => {
-    // 로그아웃
-    ctx.cookies.set('access_token');
-    ctx.status = 204; // No Content
+export const logout = async (ctx) => {
+  // 로그아웃
+  ctx.cookies.set('access_token');
+  ctx.status = 204; // No Content
 };
 
-export const remove = async ctx => {
-    const { userId } = ctx.request.body;
-    try {
-        await Post.findByIdAndRemove(userId).exec();
-        ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
-    } catch (e) {
-        ctx.throw(500, e);
-    }
+export const remove = async (ctx) => {
+  const { userId } = ctx.request.body;
+  try {
+    await Post.findByIdAndRemove(userId).exec();
+    ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
--- user-member/movie-backend/src/models/user.js
+++ user-member/movie-backend/src/models/user.js
@@ -1,57 +1,50 @@
-import mongoose, {Schema} from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
-
 const UserSchema = new Schema({
-    userId : String ,
-    hashedPassword : String,
-    username :Sting,
-    email :Sting,
-    gender :Sting,
-    nickname :Sting,
-    add :Sting,
-    tel :number,
-
-
+  userId: String,
+  hashedPassword: String,
+  username: String,
+  email: String,
+  gender: String,
+  nickname: String,
+  add: String,
+  tel: Number,
 });
 
-UserSchema.methods.setPassword = async function(password) {
-    const hash = await bcrypt.hash(password, 10);
-    this.hashedPassword = hash;
-
+UserSchema.methods.setPassword = async function (password) {
+  const hash = await bcrypt.hash(password, 10);
+  this.hashedPassword = hash;
 };
 
-UserSchema.methods.checkPassword = async function(password) {
-    const result = await bcrypt.compare(password, this.hashedPassword);
-    return result; // true / false
+UserSchema.methods.checkPassword = async function (password) {
+  const result = await bcrypt.compare(password, this.hashedPassword);
+  return result; // true / false
 };
 
-UserSchema.statics.findByUsername = function(userId) {
-    return this.findOne({ userId });
+UserSchema.statics.findByUsername = function (userId) {
+  return this.findOne({ userId });
 };
-UserSchema.methods.serialize = function(){
-    const data= this.toJSON();
-    delete data.hashedPassword;
-    return data;
-};
-
-UserSchema.methods.generateToken = function(){
-    const token =jwt.sign(
-        {
-        _id : this.id,
-            userId : this.userId,
-        },
-        process.env.JWT_SECRET,
-        {
-            expiresIn: '3d',
-        },
-    );
-    return token;
+UserSchema.methods.serialize = function () {
+  const data = this.toJSON();
+  delete data.hashedPassword;
+  return data;
 };
 
+UserSchema.methods.generateToken = function () {
+  const token = jwt.sign(
+    {
+      _id: this.id,
+      userId: this.userId,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '3d',
+    },
+  );
+  return token;
+};
 
-
-
-const User = mongoose.model('user',UserSchema);
+const User = mongoose.model('user', UserSchema);
 export default User;
